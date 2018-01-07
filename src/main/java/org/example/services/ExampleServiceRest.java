9c1676f650f17c3db9190da5727fcd25229c9af3
Xavier David Gutiérrez Rojas <xavierdavid.gr@gmail.com>
2018-01-07 15:47:59 -0300

    params example 27.0.0.1	localhost

--- src/main/java/org/example/services/ExampleServiceRest.java
+++ src/main/java/org/example/services/ExampleServiceRest.java
@@ -2,13 +2,9 @@ package org.example.services;
 
 import org.example.dto.User;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.Date;
 
 /**
  * @author Xavier Guti&#233;rrez
@@ -24,5 +20,44 @@ public class ExampleServiceRest  {
         User user = new User("i-sldaksd","Miranda");
 
         return Response.status(Response.Status.OK).entity(user).build();
+    }
+
+    @GET
+    @Path("/user/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response userInfo(@PathParam("name") String name)  {
+        User user = new User("i-sldaksd","Miranda");
+
+        user.setName(name);
+
+        return Response.status(Response.Status.OK).entity(user).build();
+    }
+
+
+    @PUT
+    @Path("/user?=name")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response userModify(@QueryParam("name") String name)  {
+        User user = new User("i-sldaksd","PUT");
+        user.setName(name);
+        return Response.status(Response.Status.OK).entity(user).build();
+    }
+
+
+    @POST
+    @Path("/user")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response userSave()  {
+        User user = new User("i-sldaksd","POST");
+
+        return Response.status(Response.Status.OK).entity(user).build();
+    }
+
+
+    @DELETE
+    @Path("/user")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response userDelete()  {
+        return Response.status(Response.Status.NO_CONTENT).build();
     }
 }
